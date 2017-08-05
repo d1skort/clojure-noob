476713e4f12b0e52454eb8e0401d0d95b8f297b4
Vladislav Pakhomov <vladislav.pakhomov@bostongene.com>
2017-08-05 17:28:58 +0300

    added network.clj and Database.clj

--- hard-db/src/task02/network.clj
+++ hard-db/src/task02/network.clj
@@ -4,43 +4,36 @@
             [clojure.string :as str])
   (:import [java.net Socket ServerSocket InetAddress InetSocketAddress SocketTimeoutException]))
 
-;; Объявить переменную для синхронизации между потоками. Воспользуйтесь promise
-(def ^:private should-be-finished :implement-me)
 
-;; Hint: *in*, *out*, io/writer, io/reader, socket.getOutputStream(), socket.getInputStream(), socket.close(), binding
-;;       deliver, prn
+(def ^:private should-be-finished (promise))
+
+
 (defn handle-request [^Socket sock]
-  (:implement-me ;; переопределить *in* & *out* чтобы они указывали на входной и выходной потоки сокета
+  (binding [*out* (io/writer sock)
+            *in* (io/reader sock)]
     (try
-      (let [s (read-line)] ;; считать данные из переопределенного *in*
+      (let [s (read-line)]
         (if (= (str/lower-case s) "quit")
-          :implement-me ;;; 1) сообщить основному потоку что мы завершаем выполнение.
-               ;;; для этого необходимо установить переменную should-be-finished в true
-               ;;;
-          :implement-me ;;; 2) выполнить запрос при помощи perform-query и записать
-               ;;; результат в переопределенный *out*
-          ))
+          (deliver should-be-finished true)
+          (println (perform-query s))))
       (catch Throwable ex
         (println "Exception: " ex))
       (finally
-        :implement-me )))) ;;; закрыть сокет
+        (.close sock)))))
 
 
-;; Hint: future, deliver
 (defn- run-loop [server-sock]
   (try
     (let [^Socket sock (.accept server-sock)]
-      :implement-me ;; выполнить функцию handle-request в отдельном потоке
-      )
+      (future (handle-request sock)))
     (catch SocketTimeoutException ex)
     (catch Throwable ex
       (println "Got exception" ex)
-      :implement-me ;; сообщить основному потоку что мы завершаем выполнение
-          ;; для этого необходимо установить переменную should-be-finished в true
-      )))
+        (deliver should-be-finished true))))
+
 
 (defn run [port]
-  (let [sock-addr (InetSocketAddress. nil port)
+  (let [sock-addr (InetSocketAddress. "localhost" port)
         server-socket (doto (ServerSocket.)
                         (.setReuseAddress true)
                         (.setSoTimeout 3000)
